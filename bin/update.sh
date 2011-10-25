ca8eca18f9ee7e6201778a40eb91fa46a3c90d9b
Austin King <shout@ozten.com>
2011-10-25 04:14:31 -0700

    Adding sed to rewrite hostnames for deployment environments.

---
+++ bin/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Usage: Edit then run update.sh. Changes JavaScript to  rewrite hostnames.
+# Edit these values to match your environment
+REPO_PATH=/home/ozten/Projects/repotheweb
+HOSTNAME=dev.repotheweb.org:8001
+
+# Do not edit below this line
+cd $REPO_PATH
+git checkout www/repotheweb.org/include.js
+git pull
+FILES='www/repotheweb.org/include.js www/rhapsody.com/index.html www/music-blog.com/index.html'
+for file in $FILES; do
+    sed -e"s/repotheweb\.org/${HOSTNAME}/g" $file > ${file}.tmp
+    mv ${file}.tmp $file
+done
