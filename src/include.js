6aecf65677f0b7e920dc756f9af00005fadc5fd6
alcinnz <alcinnz@eml.cc>
2011-11-10 22:00:14 +1300

    Update src/include.js

--- src/include.js
+++ src/include.js
@@ -3,61 +3,59 @@
 
 require(
     ['json-storage', 'jschannels', 'simulator', 'utils', 'config'], 
-    function (jschannels, sim, utils, config) {
-        var init = function () {
-            if (!navigator.xregisterProtocolHandler || !navigator._registerProtocolHandlerIsShimmed) {
-                var simulate_rph;
-                navigator.xregisterProtocolHandler = function (scheme, url, title) {
-                    // Prompt user, if conset, store locally
-                    var domain_parts, domain,
-                    doc = window.document,
-                    iframe = utils._open_hidden_iframe(doc),
-                    chan = config.chan;
+    function (store, jschannels, sim, utils, config) {
+        if (!navigator.xregisterProtocolHandler || !navigator._registerProtocolHandlerIsShimmed) {
+            var simulate_rph;
+            navigator.xregisterProtocolHandler = function (scheme, url, title) {
+                // Prompt user, if conset, store locally
+                var domain_parts, domain,
+                doc = window.document,
+                iframe = utils._open_hidden_iframe(doc),
+                chan = config.chan;
 
-                    // clean up a previous channel that never was reaped
-                    if (chan) chan.destroy();
-                    chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
+                // clean up a previous channel that never was reaped
+                if (chan) chan.destroy();
+                chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
 
-                    function cleanup() {
-                        chan.destroy();
-                        chan = undefined;
-                        if (iframe.close) iframe.close();
-                        iframe.parentNode.removeChild(iframe);
-                    }
-
-                    if (url.indexOf("%s") == -1) {
-                        if (window.console) console.error("url missing %s " + url);
-                        return;
-                    }
-                    domain_parts = url.split('/');
-                    if (domain_parts.length < 2) {
-                        if (window.console) console.error("Improper url " + url);
-                        return;
-                    }
-                    domain = domain_parts[2];
-                    // Simulate hanger notification
-                    if (confirm("Add " + title + "(" + domain + ") as an application for " +
-                                scheme + " links?")) {
-                        chan.call({
-                                      method: "registerProtocolHandler",
-                                      params: {scheme: scheme, url: url, title:title, default:true},
-                                      success: function (rv) {
-                                          cleanup();
-                                      },
-                                      error: function(code, msg) {
-
-                                      }
-                                  });//chan.call
-                    }// if confirm
-                    navigator._registerProtocolHandlerIsShimmed = true;
-
+                function cleanup() {
+                    chan.destroy();
+                    chan = undefined;
+                    if (iframe.close) iframe.close();
+                    iframe.parentNode.removeChild(iframe);
                 }
 
-                $('a').click(sim.simulate_rph);
+                if (url.indexOf("%s") == -1) {
+                    if (window.console) console.error("url missing %s " + url);
+                    return;
+                }
+                domain_parts = url.split('/');
+                if (domain_parts.length < 2) {
+                    if (window.console) console.error("Improper url " + url);
+                    return;
+                }
+                domain = domain_parts[2];
+                // Simulate hanger notification
+                if (confirm("Add " + title + "(" + domain + ") as an application for " +
+                            scheme + " links?")) {
+                    chan.call({
+                                  method: "registerProtocolHandler",
+                                  params: {scheme: scheme, url: url, title:title, default:true},
+                                  success: function (rv) {
+                                      cleanup();
+                                  },
+                                  error: function(code, msg) {
 
-            } // end if
-        }; // init
-        $(document).ready(function () {
-                              init();
-                          });
+                                  }
+                              });//chan.call
+                }// if confirm
+                navigator._registerProtocolHandlerIsShimmed = true;
+
+            }
+
+            document.onclick = function(e) {
+                e |= window.event;
+                
+            }
+
+        } // end if
     }); // end require
