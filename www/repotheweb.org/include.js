516e858ec8ddb6fa5e5802f1c6515b6e96611c22
Austin King <shout@ozten.com>
2011-11-09 23:02:53 -0800

    Fixed confusion between lists and assoc arrays in localStorage.

--- www/repotheweb.org/include.js
+++ www/repotheweb.org/include.js
@@ -774,7 +774,7 @@ define('jschannels', {
   // TODO include browserid/static/resources/jschannels.js
 });
 define('config', {
-  ipServer: 'http://repotheweb.org',
+  ipServer: 'http://dev.repotheweb.org:8001',
   chan: undefined
 });
 define('utils', ['config'], 
@@ -822,7 +822,6 @@ define(
                 // ensure handler_list exists
                 e.preventDefault();
                 fallback = $('meta[name=fallback-rph][protocol=' + this_scheme + ']', $(this).parents('html')).attr('content');
-                alert(fallback);
                 _.run_protocol_handler(this_scheme, this_url, fallback);
                 return true;
             }, /* simulate_rph */
@@ -881,65 +880,72 @@ define(
 /*jslint strict: false, plusplus: false */
 /*global require: true, navigator: true, window: true */
 
-require(
-    ['jschannels', 'simulator', 'utils', 'config'], 
+define('initalize',['jschannels', 'simulator', 'utils', 'config'], 
     function (jschannels, sim, utils, config) {
-        var init = function () {
-            if (!navigator.xregisterProtocolHandler || !navigator._registerProtocolHandlerIsShimmed) {
-                var simulate_rph;
-                navigator.xregisterProtocolHandler = function (scheme, url, title) {
-                    // Prompt user, if conset, store locally
-                    var domain_parts, domain,
-                    doc = window.document,
-                    iframe = utils._open_hidden_iframe(doc),
-                    chan = config.chan;
 
-                    // clean up a previous channel that never was reaped
-                    if (chan) chan.destroy();
-                    chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
+        return {
+            init: function () {
+                if (!navigator.xregisterProtocolHandler || !navigator._registerProtocolHandlerIsShimmed) {
+                    var simulate_rph;
+                    navigator.xregisterProtocolHandler = function (scheme, url, title) {
+                        // Prompt user, if conset, store locally
+                        var domain_parts, domain,
+                        doc = window.document,
+                        iframe = utils._open_hidden_iframe(doc),
+                        chan = config.chan;
 
-                    function cleanup() {
-                        chan.destroy();
-                        chan = undefined;
-                        if (iframe.close) iframe.close();
-                        iframe.parentNode.removeChild(iframe);
-                    }
+                        // clean up a previous channel that never was reaped
+                        if (chan) chan.destroy();
+                        chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
 
-                    if (url.indexOf("%s") == -1) {
-                        if (window.console) console.error("url missing %s " + url);
-                        return;
-                    }
-                    domain_parts = url.split('/');
-                    if (domain_parts.length < 2) {
-                        if (window.console) console.error("Improper url " + url);
-                        return;
-                    }
-                    domain = domain_parts[2];
-                    // Simulate hanger notification
-                    if (confirm("Add " + title + "(" + domain + ") as an application for " +
-                                scheme + " links?")) {
-                        chan.call({
-                                      method: "registerProtocolHandler",
-                                      params: {scheme: scheme, url: url, title:title, default:true},
-                                      success: function (rv) {
-                                          cleanup();
-                                      },
-                                      error: function(code, msg) {
+                        function cleanup() {
+                            chan.destroy();
+                            chan = undefined;
+                            if (iframe.close) iframe.close();
+                            iframe.parentNode.removeChild(iframe);
+                        }
 
-                                      }
-                                  });//chan.call
-                    }// if confirm
-                    navigator._registerProtocolHandlerIsShimmed = true;
+                        if (url.indexOf("%s") == -1) {
+                            if (window.console) console.error("url missing %s " + url);
+                            return;
+                        }
+                        domain_parts = url.split('/');
+                        if (domain_parts.length < 2) {
+                            if (window.console) console.error("Improper url " + url);
+                            return;
+                        }
+                        domain = domain_parts[2];
+                        // Simulate hanger notification
+                        if (confirm("Add " + title + "(" + domain + ") as an application for " +
+                                    scheme + " links?")) {
+                            chan.call({
+                                          method: "registerProtocolHandler",
+                                          params: {scheme: scheme, url: url, title:title, default:true},
+                                          success: function (rv) {
+                                              cleanup();
+                                          },
+                                          error: function(code, msg) {
 
-                }
+                                          }
+                                      });//chan.call
+                        }// if confirm
+                        navigator._registerProtocolHandlerIsShimmed = true;
 
-                $('a').click(sim.simulate_rph);
+                    };
+                    $('a').click(sim.simulate_rph);
 
-            } // end if
-        }; // init
-        $(document).ready(function () {
-                              init();
-                          });
-    }); // end require;
+                } // end if
+            } // init
+        };
+    }); // end require
+;
+/*jslint strict: false, plusplus: false */
+/*global require: true, navigator: true, window: true */
+
+
+require(['initalize'], 
+        function (initalize) {
+            $(document).ready(initalize.init);
+        });
 define("include", function(){});
 }());
