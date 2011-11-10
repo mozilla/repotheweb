b2c06558391905a179f2757ac83881d666bde75b
Austin King <shout@ozten.com>
2011-11-09 21:24:01 -0800

    Playing with TC's meta tag idea for a fallback protocol handler

--- src/simulator.js
+++ src/simulator.js
@@ -6,23 +6,32 @@ define(
             /* BEGIN registerProtocolHandler simulator
              *
              * When a user clicks a link that isn't a standard
-             * internet protocol, this code will look in localStorage for a registered protocol handler. If one is found, the href of the link is re-written to the handler's url.
+             * internet protocol, this code will:
+             * 1) Look in localStorage for a registered protocol handler
+             * 2) Look in the current html document for a fallback meta tag
+             *
+             * If a protocol handler is found, the href of the link is
+             * re-written to the handler's url.
+             *
+             * If none are found, pass-through to the browser
              */
             simulate_rph: function (e) {
                 var this_url = $(this).attr('href'),
-                this_scheme = this_url.split(':')[0],
-                official_schemes = [
+                    this_scheme = this_url.split(':')[0],
+                    official_schemes = [
                     'http', 'https', 'mailto', 'ftp', 'gopher'
-                ], //gopher, I kid, I kid
-                prtcl_hndlr,
-                _ = require('simulator');
+                    ], //gopher, I kid, I kid
+                    prtcl_hndlr,
+                    _ = require('simulator'),
+                    fallback;
                 e.preventDefault();
                 if (this_url.indexOf(official_schemes) != -1) {
                     return false;
                 }
                 // ensure handler_list exists
                 e.preventDefault();
-                _.run_protocol_handler(this_scheme, this_url);
+                fallback = $('meta[name=fallback-rph][protocol=' + this_scheme + ']', $(this).parents('html')).attr('content');
+                _.run_protocol_handler(this_scheme, this_url, fallback);
                 return true;
             }, /* simulate_rph */
             /**
@@ -33,7 +42,7 @@ define(
              * orig_url - string - The original non-standard URL
              * Return - void, async
              */
-            run_protocol_handler: function (scheme, orig_url) {
+            run_protocol_handler: function (scheme, orig_url, fallback) {
                 var iframe = utils._open_hidden_iframe(window.document),
                 chan = config.chan;
                 // clean up a previous channel that never was reaped
@@ -48,22 +57,32 @@ define(
                 };
 
                 // TODO caller semantics... async or sync. Leak handlers into 3rd party sites to pre-load?
-                chan.call({
-                              method: "protocolHandler",
-                              params: {scheme: scheme, url: orig_url},
-                              success: function (new_url) {
+                chan.call(
+                    {
+                        method: "protocolHandler",
+                        params: {scheme: scheme, url: orig_url},
+                        success: function (new_url) {
+                            //TODO rph_iframe.js DRY
+                            var rewrite_url = function (handler_url, url) {
+                                    var parts = handler_url.split('%s');
+                                    return parts[0] + url + parts[1];
+                                },
+                                fallback_url = rewrite_url(fallback, orig_url);
+                            cleanup();
+                            if (new_url === false) {
+                                if (fallback) {
+                                    window.location = fallback_url;
+                                } else {
+                                    window.location = orig_url;
+                                }
+                            } else {
+                                window.location = new_url;
+                            }
+                        },
+                        error: function(code, msg) {
 
-                                  cleanup();
-                                  if (new_url === false) {
-                                      window.location = orig_url;
-                                  } else {
-                                      window.location = new_url;
-                                  }
-                              },
-                              error: function(code, msg) {
-
-                              }
-                          }); //chan.call
+                        }
+                    }); //chan.call
             } /* run_protocol_handler */
-        }
+        };
     });
--- www/repotheweb.org/include.js
+++ www/repotheweb.org/include.js
@@ -777,93 +777,66 @@ define('config', {
   ipServer: 'http://repotheweb.org',
   chan: undefined
 });
-define('utils', ['config'], function (config) {
-    return {
-        _open_hidden_iframe: function (doc) {
-            var iframe = doc.createElement("iframe");
-            // iframe.style.display = "none";
-            doc.body.appendChild(iframe);
-            iframe.src = config.ipServer + "/rph_iframe.html";
-            return iframe;
-        }
-    };
-});
-define('simulator', ['config', 'jschannels', 'utils'], function (config, jschannels, utils) {
-    return {        
-      /* BEGIN registerProtocolHandler simulator
-       *
-       * When a user clicks a link that isn't a standard
-       * internet protocol, this code will look in localStorage for a registered protocol handler. If one is found, the href of the link is re-written to the handler's url.
-       */
-      simulate_rph: function (e) {        
-        var this_url = $(this).attr('href'),
-            this_scheme = this_url.split(':')[0],
-            official_schemes = [
-                'http', 'https', 'mailto', 'ftp', 'gopher'
-            ], //gopher, I kid, I kid
-            prtcl_hndlr,
-            _ = require('simulator');
-e.preventDefault();
-        if (this_url.indexOf(official_schemes) != -1) {
-            return false;
-        }
-        // ensure handler_list exists
-        e.preventDefault();
-        _.run_protocol_handler(this_scheme, this_url);        
-      }, /* simulate_rph */
-      /**
-       * Asynchonous function to run the user's protocol handler or passthrough to browser. 
-       * Method cancels the current event.
-       *
-       * scheme - string - A scheme for a non-standard URI
-       * orig_url - string - The original non-standard URL
-       * Return - void, async
-       */
-      run_protocol_handler: function (scheme, orig_url) {
-        var iframe = utils._open_hidden_iframe(window.document),
-            chan = config.chan;
-        // clean up a previous channel that never was reaped
-        if (chan) chan.destroy();
-        chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
-
-        function cleanup() {
-            chan.destroy();
-            chan = undefined;
-            if (iframe.close) iframe.close();
-            if (iframe.parentNode) iframe.parentNode.removeChild(iframe);
-        };
-
-        // TODO caller semantics... async or sync. Leak handlers into 3rd party sites to pre-load?
-        chan.call({
-            method: "protocolHandler",
-            params: {scheme: scheme, url: orig_url},
-            success: function (new_url) {
-
-                cleanup();
-                if (new_url === false) {
-                    window.location = orig_url;
-                } else {
-                    window.location = new_url;
+define('utils', ['config'], 
+       function (config) {
+           return {
+               _open_hidden_iframe: function (doc) {
+                   var iframe = doc.createElement("iframe");
+                   // iframe.style.display = "none";
+                   doc.body.appendChild(iframe);
+                   iframe.src = config.ipServer + "/rph_iframe.html";
+                   return iframe;
+               }
+           };
+       });
+define(
+    'simulator',
+    ['config', 'jschannels', 'utils'],
+    function (config, jschannels, utils) {
+        return {
+            /* BEGIN registerProtocolHandler simulator
+             *
+             * When a user clicks a link that isn't a standard
+             * internet protocol, this code will:
+             * 1) Look in localStorage for a registered protocol handler
+             * 2) Look in the current html document for a fallback meta tag
+             *
+             * If a protocol handler is found, the href of the link is
+             * re-written to the handler's url.
+             *
+             * If none are found, pass-through to the browser
+             */
+            simulate_rph: function (e) {
+                var this_url = $(this).attr('href'),
+                    this_scheme = this_url.split(':')[0],
+                    official_schemes = [
+                    'http', 'https', 'mailto', 'ftp', 'gopher'
+                    ], //gopher, I kid, I kid
+                    prtcl_hndlr,
+                    _ = require('simulator'),
+                    fallback;
+                e.preventDefault();
+                if (this_url.indexOf(official_schemes) != -1) {
+                    return false;
                 }
-            },
-            error: function(code, msg) {
-
-            }
-        }); //chan.call
-      } /* run_protocol_handler */
-    }
-});
-require(['jschannels', 'simulator', 'utils', 'config'], function (jschannels, sim, utils, config) {
-    var init = function () {    
-        if (!navigator.xregisterProtocolHandler || !navigator._registerProtocolHandlerIsShimmed) {
-            var simulate_rph;
-            navigator.xregisterProtocolHandler = function (scheme, url, title) {
-                // Prompt user, if conset, store locally
-                var domain_parts, domain,
-                    doc = window.document,
-                    iframe = utils._open_hidden_iframe(doc),
-                    chan = config.chan;
-
+                // ensure handler_list exists
+                e.preventDefault();
+                fallback = $('meta[name=fallback-rph][protocol=' + this_scheme + ']', $(this).parents('html')).attr('content');
+                alert(fallback);
+                _.run_protocol_handler(this_scheme, this_url, fallback);
+                return true;
+            }, /* simulate_rph */
+            /**
+             * Asynchonous function to run the user's protocol handler or passthrough to browser.
+             * Method cancels the current event.
+             *
+             * scheme - string - A scheme for a non-standard URI
+             * orig_url - string - The original non-standard URL
+             * Return - void, async
+             */
+            run_protocol_handler: function (scheme, orig_url, fallback) {
+                var iframe = utils._open_hidden_iframe(window.document),
+                chan = config.chan;
                 // clean up a previous channel that never was reaped
                 if (chan) chan.destroy();
                 chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
@@ -872,44 +845,101 @@ require(['jschannels', 'simulator', 'utils', 'config'], function (jschannels, si
                     chan.destroy();
                     chan = undefined;
                     if (iframe.close) iframe.close();
-                    iframe.parentNode.removeChild(iframe);
-                }
+                    if (iframe.parentNode) iframe.parentNode.removeChild(iframe);
+                };
 
-                if (url.indexOf("%s") == -1) {
-                    if (window.console) console.error("url missing %s " + url);
-                    return;
-                }
-                domain_parts = url.split('/');
-                if (domain_parts.length < 2) {
-                    if (window.console) console.error("Improper url " + url);
-                    return;
-                }
-                domain = domain_parts[2];
-                // Simulate hanger notification
-                if (confirm("Add " + title + "(" + domain + ") as an application for " +
-                            scheme + " links?")) {
-                    chan.call({
-                        method: "registerProtocolHandler",
-                        params: {scheme: scheme, url: url, title:title, default:true},
-                        success: function (rv) {
+                // TODO caller semantics... async or sync. Leak handlers into 3rd party sites to pre-load?
+                chan.call(
+                    {
+                        method: "protocolHandler",
+                        params: {scheme: scheme, url: orig_url},
+                        success: function (new_url) {
+                            //TODO rph_iframe.js DRY
+                            var rewrite_url = function (handler_url, url) {
+                                    var parts = handler_url.split('%s');
+                                    return parts[0] + url + parts[1];
+                                },
+                                fallback_url = rewrite_url(fallback, orig_url);
                             cleanup();
+                            if (new_url === false) {
+                                if (fallback) {
+                                    window.location = fallback_url;
+                                } else {
+                                    window.location = orig_url;
+                                }
+                            } else {
+                                window.location = new_url;
+                            }
                         },
                         error: function(code, msg) {
-                                      
+
                         }
-                    });//chan.call
-                }// if confirm
-                navigator._registerProtocolHandlerIsShimmed = true;    
-
-            }
-
-            $('a').click(sim.simulate_rph);
-    
-        } // end if
-    }; // init
-    $(document).ready(function () {
-        init();
+                    }); //chan.call
+            } /* run_protocol_handler */
+        };
     });
-}); // end require;
+/*jslint strict: false, plusplus: false */
+/*global require: true, navigator: true, window: true */
+
+require(
+    ['jschannels', 'simulator', 'utils', 'config'], 
+    function (jschannels, sim, utils, config) {
+        var init = function () {
+            if (!navigator.xregisterProtocolHandler || !navigator._registerProtocolHandlerIsShimmed) {
+                var simulate_rph;
+                navigator.xregisterProtocolHandler = function (scheme, url, title) {
+                    // Prompt user, if conset, store locally
+                    var domain_parts, domain,
+                    doc = window.document,
+                    iframe = utils._open_hidden_iframe(doc),
+                    chan = config.chan;
+
+                    // clean up a previous channel that never was reaped
+                    if (chan) chan.destroy();
+                    chan = jschannels.Channel.build({window: iframe.contentWindow, origin: '*', scope: "mozid"});
+
+                    function cleanup() {
+                        chan.destroy();
+                        chan = undefined;
+                        if (iframe.close) iframe.close();
+                        iframe.parentNode.removeChild(iframe);
+                    }
+
+                    if (url.indexOf("%s") == -1) {
+                        if (window.console) console.error("url missing %s " + url);
+                        return;
+                    }
+                    domain_parts = url.split('/');
+                    if (domain_parts.length < 2) {
+                        if (window.console) console.error("Improper url " + url);
+                        return;
+                    }
+                    domain = domain_parts[2];
+                    // Simulate hanger notification
+                    if (confirm("Add " + title + "(" + domain + ") as an application for " +
+                                scheme + " links?")) {
+                        chan.call({
+                                      method: "registerProtocolHandler",
+                                      params: {scheme: scheme, url: url, title:title, default:true},
+                                      success: function (rv) {
+                                          cleanup();
+                                      },
+                                      error: function(code, msg) {
+
+                                      }
+                                  });//chan.call
+                    }// if confirm
+                    navigator._registerProtocolHandlerIsShimmed = true;
+
+                }
+
+                $('a').click(sim.simulate_rph);
+
+            } // end if
+        }; // init
+        $(document).ready(function () {
+                              init();
+                          });
+    }); // end require;
 define("include", function(){});
 }());
