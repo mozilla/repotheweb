b2478bbfe461b85cbcdb81d5b38a5ff0fe6447a1
Adrian Cochrane <alcinnz@eml.cc>
2011-12-31 20:39:52 +1300

    Improving UI framework, preparing for select prompt which will iron out remaining bugs and dramatically increasing UX.

---
+++ www/repotheweb.org/utils.js
@@ -0,0 +1,55 @@
+var _ = function() {
+	    var args = Array.prototype.slice.call(arguments),
+        string = _.lang[args.shift()].substr();
+
+        for (var i=0;i<args.length;i++) string.replace('%s', args[i]);
+        return string;
+    },
+    // From John Resig, at http://ejohn.org/blog/javascript-micro-templating/
+    // Slightly modified for purpouses of this project
+    // No caching (due to usage) and uses {{ & }} (due to Django taste)
+    tmpl = function tmpl(str, data){
+        // Figure out if we're getting a template, or if we need to
+        // load the template - and be sure to cache the result.
+        var fn = !/\W/.test(str) ?
+            tmpl(document.getElementById(str).innerHTML) :
+     
+          // Generate a reusable function that will serve as a template
+          // generator (and which will be cached).
+          new Function("obj",
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+           
+            // Introduce the data as local variables using with(){}
+            "with(obj){p.push('" +
+           
+            // Convert the template into pure JavaScript
+            str
+              .replace(/[\r\t\n]/g, " ")
+              .split("{{").join("\t")
+              .replace(/((^|}})[^\t]*)'/g, "$1\r")
+              .replace(/\t=(.*?)}}/g, "',$1,'")
+              .split("\t").join("');")
+              .split("}}").join("p.push('")
+              .split("\r").join("\\'")
+          + "');}return p.join('');");
+       
+        // Provide some basic currying to the user
+        return data ? fn( data ) : fn;
+      },
+    data = function(key) {
+        return JSON.parse(localStorage.getItem(key));
+    },
+    each = $.each;
+
+$(function() {
+    $('script[type="text/template"]').each(function() {
+        var html = tmpl($(this).html(), window);
+        $(this).replaceWith(html);
+    });
+});
+
+// Temporary english translations, will be moved out once defined
+_.lang = {
+    'Protocol Handlers' : 'Protocol Handlers'
+}
+        
